a61119075308740e70fa3b2c3fe3b011342b620e
Victor Williams <vrwilliams719@gmail.com>
2020-02-21 09:38:14 -0500

    changes

--- src/math/PrimeNumberFinder.java
+++ src/math/PrimeNumberFinder.java
@@ -1,40 +1,39 @@
 package math;
+import java.util.ArrayList;
 /**
  *
  * @author Victor Williams/todoslosdays
  */
 
-import java.util.ArrayList;
-
-public class PrimeNumbers {
+public class Prime-Number-Finder {
 
 	public static void main(String[] args) {
 
 		ArrayList<Long> prime = new ArrayList<Long>();
-		boolean isPrime = true;
+		boolean primeExists = true;
 
 		long limit = 9_223_372_036_854_775_806L;
 		for (long i = 2; i < limit; i++) {
 
-			isPrime = checkPrime(i);
+			primeExists = checkForPrime(i);
 
-			if (isPrime) {
+			if (primeExists) {
 				prime.add(i); System.out.println(i + " is a prime number");
-				/*if (checkPrime((long) (Math.pow(2, i) - 1))) {
+				/*if (checkForPrime((long) (Math.pow(2, i) - 1))) {
 					System.out.println("Prime number " + i + " is a Mersenne prime.");
 				}*/
 			}
 		}
 	}
 
-	public static boolean checkPrime(long d) {
-		boolean isPrime = true;
+	public static boolean checkForPrime(long d) {
+		boolean primeExists = true;
 		for (long j = 2; j <= d - 1; j++) {
 			if (d % j == 0) {
-				isPrime = false;
+				primeExists = false;
 				break;
 			}
 		}
-		return isPrime;
+		return primeExists;
 	}
 }
